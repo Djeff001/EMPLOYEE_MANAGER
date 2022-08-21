c709463c9f665c34dd900d018592b1f1b655f3d7
Djeff <nait.sider.djaffer@gmail.com>
2022-08-22 00:50:54 +0200

    add backend part

---
+++ src/main/java/tech/getarrays/employeemanager/exception/UserNotFounException.java
@@ -0,0 +1,7 @@
+package tech.getarrays.employeemanager.exception;
+
+public class UserNotFounException extends RuntimeException{
+    public UserNotFounException(String message) {
+        super(message);
+    }
+}
